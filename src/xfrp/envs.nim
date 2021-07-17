b237159f01ccdf9d3fa10187a13b3e2aae798b22
Double-oxygeN <mail@double-oxygen.net>
2021-07-17 15:56:11 +0900

    add reference info into node environment

--- src/xfrp/envs.nim
+++ src/xfrp/envs.nim
@@ -1,4 +1,5 @@
 import tables, options
+from sequtils import deduplicate, mapIt, unzip, concat
 import patty
 import syntax, types, codeinfos, errors
 
@@ -14,24 +15,71 @@ type
     typeOpt: Option[WithCodeInfo[XfrpType]]
     init: Option[WithCodeInfo[XfrpExpr]]
     update: WithCodeInfo[XfrpExpr]
+    # additional info.
+    refNow, refAtLast: seq[XfrpId]
 
   XfrpEnv* = object
     name: WithCodeInfo[XfrpModuleId]
-    materials: TableRef[XfrpModuleId, XfrpEnv]
+    materials: TableRef[XfrpModuleId, ref XfrpEnv]
     funcs: TableRef[XfrpId, XfrpFuncDefinition]
     innerNodes: TableRef[XfrpId, XfrpNodeDefinition]
     inputNodes: OrderedTableRef[XfrpId, WithCodeInfo[XfrpIdAndType]]
     outputNodes: seq[WithCodeInfo[XfrpIdAndTypeOpt]]
 
+  NodeReferenceInfo = tuple
+    refNow, refAtLast: seq[XfrpId]
+
+
+func extractNodeReferenceInfo(env: XfrpEnv; exp: XfrpExpr): NodeReferenceInfo =
+  match exp:
+    ExprLiteral(_):
+      return
+
+    ExprId(idAst):
+      let id = idAst.val
+      if (id in env.inputNodes) or (id in env.innerNodes):
+        result.refNow = @[id]
+
+    ExprAnnot(idAst, annotAst):
+      let id = idAst.val
+      match annotAst:
+        AnnotAtLast:
+          if (id in env.inputNodes) or (id in env.innerNodes):
+            result.refAtLast = @[id]
+
+    ExprBin(_, lhsAst, rhsAst):
+      let
+        (lhsRefNow, lhsRefAtLast) = env.extractNodeReferenceInfo(lhsAst[].val)
+        (rhsRefNow, rhsRefAtLast) = env.extractNodeReferenceInfo(rhsAst[].val)
+
+      result.refNow = deduplicate(lhsRefnow & rhsRefNow)
+      result.refAtLast = deduplicate(lhsRefAtLast & rhsRefAtLast)
+
+    ExprIf(ifAst, thenAst, elseAst):
+      let
+        (ifRefNow, ifRefAtLast) = env.extractNodeReferenceInfo(ifAst[].val)
+        (thenRefNow, thenRefAtLast) = env.extractNodeReferenceInfo(thenAst[].val)
+        (elseRefNow, elseRefAtLast) = env.extractNodeReferenceInfo(elseAst[].val)
+
+      result.refNow = deduplicate(ifRefNow & thenRefNow & elseRefNow)
+      result.refAtLast = deduplicate(ifRefAtLast & thenRefAtLast & elseRefAtLast)
+
+    ExprApp(_, argAsts):
+      let (refsNow, refsAtLast) = argAsts.mapIt(env.extractNodeReferenceInfo(it.val)).unzip()
+      result.refNow = deduplicate(concat(refsNow))
+      result.refAtLast = deduplicate(concat(refsAtLast))
+
+
 proc makeEnvironmentFromModule*(ast: XfrpModule): XfrpEnv =
   # Initialization
   result.name = ast.moduleId
-  # result.materials = newTable[XfrpId, XfrpEnv]()
+  # result.materials = newTable[XfrpId, ref XfrpEnv]()
   result.funcs = newTable[XfrpId, XfrpFuncDefinition]()
   result.innerNodes = newTable[XfrpId, XfrpNodeDefinition]()
   result.inputNodes = newOrderedTable[XfrpId, WithCodeInfo[XfrpIdAndType]]()
   result.outputNodes = ast.outs
 
+  # Registration of input nodes
   for node in ast.ins:
     let
       (idAst, _) = node.val
@@ -46,6 +94,7 @@ proc makeEnvironmentFromModule*(ast: XfrpModule): XfrpEnv =
 
     result.inputNodes[id] = node
 
+  # Registration of inner nodes
   for def in ast.defs:
     match def.val:
       DefNode(idAndTypeOptAst, initAstOpt, bodyAst):
@@ -80,6 +129,11 @@ proc makeEnvironmentFromModule*(ast: XfrpModule): XfrpEnv =
           raise err
 
         result.funcs[id] = XfrpFuncDefinition(id: idAst, retType: retTypeAst, args: argAsts, body: bodyAst)
+
+  # Extract node references
+  for nodeDef in mvalues(result.innerNodes):
+    (nodeDef.refNow, nodeDef.refAtLast) = result.extractNodeReferenceInfo(nodeDef.update.val)
+
 
 when isMainModule:
   import os, json, std/jsonutils
