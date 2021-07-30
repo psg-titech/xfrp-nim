39fbfa1d69be26178f648e797bb913c0763a2496
Double-oxygeN <mail@double-oxygen.net>
2021-07-30 11:36:36 +0900

    WIP: change loaders

--- src/xfrp/loaders.nim
+++ src/xfrp/loaders.nim
@@ -1,26 +1,28 @@
 ## XFRP module loader.
 
 import tables, os
-import lexer, parser, syntax, errors
+import lexer, parser, syntax, errors, codeinfos
 
 type
   XfrpLoader* = ref object
-    loaded: TableRef[XfrpModuleId, XfrpAst[XfrpModule]]
-    includePaths: seq[string]
+    loaded: TableRef[XfrpModuleId, WithCodeInfo[XfrpModule]]
+    includeDirs: seq[string]
+
+  XfrpMaterials* = TableRef[XfrpModuleId, WithCodeInfo[XfrpModule]]
 
 
-proc newXfrpLoader*(includePaths: seq[string]): XfrpLoader =
+proc newXfrpLoader*(includeDirs: seq[string]): XfrpLoader =
   new result
-  result.loaded = newTable[XfrpModuleId, XfrpAst[XfrpModule]]()
-  result.includePaths = includePaths
+  result.loaded = newTable[XfrpModuleId, WithCodeInfo[XfrpModule]]()
+  result.includeDirs = includeDirs
 
 
-proc loadFromExistingFile(filePath: string): XfrpAst[XfrpModule] =
+proc loadFromExistingFile(filePath: string): WithCodeInfo[XfrpModule] =
   var l = buildLexerFromFilename(filePath)
   return parse(l)
 
 
-proc load*(loader: XfrpLoader; name: string; traversal = true): XfrpAst[XfrpModule] =
+proc load*(loader: XfrpLoader; name: string; traverseIncludeDirs = true): WithCodeInfo[XfrpModule] =
   if name in loader.loaded:
     return loader.loaded[name]
 
@@ -42,8 +44,8 @@ proc load*(loader: XfrpLoader; name: string; traversal = true): XfrpAst[XfrpModu
   else:
     searchPaths.add moduleDir / moduleFileName
 
-  if traversal:
-    for includePath in loader.includePaths:
+  if traverseIncludeDirs:
+    for includePath in loader.includeDirs:
       let filePath = includePath / moduleDir / moduleFileName
 
       if fileExists(filePath):
@@ -57,3 +59,59 @@ proc load*(loader: XfrpLoader; name: string; traversal = true): XfrpAst[XfrpModu
   let err = XfrpLoadError.newException("File not found.")
   err.searchPaths = searchPaths
   raise err
+
+
+proc loadMaterials*(loader: XfrpLoader; ast: WithCodeInfo[XfrpModule]; history: seq[XfrpModuleId] = @[]): XfrpMaterials =
+  result = newTable[XfrpModuleId, WithCodeInfo[XfrpModule]]()
+
+  for depModule in ast.val.uses:
+    if depModule.val in history:
+      let err = XfrpReferenceError.newException("Cyclic module dependency is detected.")
+      err.causedBy(depModule)
+      raise err
+
+    try:
+      let
+        depAst = loader.load(depModule.val)
+        depMaterials = loader.loadMaterials(depAst, history & ast.val.moduleId.val)
+
+      result[depModule.val] = depAst
+      for (depMaterialId, depMaterialAst) in pairs(depMaterials):
+        result[depMaterialId] = depMaterialAst
+
+    except XfrpLoadError as err:
+      err.causedBy(depModule)
+      raise err
+
+
+when isMainModule:
+  import os, json, std/jsonutils
+
+  if paramCount() < 1:
+    stderr.writeLine "Usage: loaders [filename]"
+    quit QuitFailure
+
+  try:
+    let
+      loader = newXfrpLoader(@[getCurrentDir()])
+      ast = loader.load(paramStr(1), false)
+      materials = loader.loadMaterials(ast)
+
+    echo pretty((ast: ast, materials: materials).toJson())
+
+  except XfrpLoadError as err:
+    stderr.writeLine "[", err.name, "] ", err.msg
+
+    stderr.writeLine "Search:"
+    for searchPath in err.searchPaths:
+      stderr.writeLine "  ", searchPath
+    stderr.writeLine ""
+
+    for info in err.causes:
+      stderr.writeLine pretty(info)
+
+  except XfrpLanguageError as err:
+    stderr.writeLine "[", err.name, "] ", err.msg
+
+    for info in err.causes:
+      stderr.writeLine pretty(info)
